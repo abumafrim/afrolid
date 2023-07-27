fcba4c97850a74cf7a088697d3a88d4a9aec4cc4
Idris Abdulmumin <abumafrim@gmail.com>
2023-07-27 10:49:51 +0100

    Update main.py

--- afrolid/main.py
+++ afrolid/main.py
@@ -27,7 +27,8 @@ class classifier():
     models, _model_args = checkpoint_utils.load_model_ensemble([self.model_path+"/afrolid_v1_checkpoint.pt"], task=afrolid_task)
     model = models[0]
     if torch.cuda.is_available():
-      model.to('cuda')
+      device = torch.device("cuda")
+      model.cuda()
     model.eval()
     tokenizer = spm.SentencePieceProcessor()
     tokenizer.Load(self.model_path+"/afrolid_spm_517_bpe.model")
